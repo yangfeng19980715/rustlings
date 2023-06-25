aa01464ef1b976ccb363b1b86a50a28bf098fe8d
yangfeng19980715 <yangfeng19980715@gmail.com>
2023-06-25 22:30:45 +0800

    part of iterator

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -15,7 +15,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_uppercase().to_string(),
     }
 }
 
@@ -24,7 +24,8 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter().map(|s| capitalize_first(s)).collect()
+    // vec![]
 }
 
 // Step 3.
