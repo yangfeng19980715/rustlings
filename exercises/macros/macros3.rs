5bcc2c553c066def408ec92fe9a2b43b05dba250
yangfeng19980715 <yangfeng19980715@gmail.com>
2023-06-29 23:12:57 +0800

    part of exercise

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,15 +5,16 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 mod macros {
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
 }
+
+// use crate::macros::my_macro;
 
 fn main() {
     my_macro!();
